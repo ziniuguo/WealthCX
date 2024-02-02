0bdf3542bb1875087c8c88a2d56b7154cda5fcc7
Liu Lingao <lingaoliu105@gmail.com>
2024-02-02 11:07:18 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if ! command -v npm &> /dev/null; then
+  echo "npm is not installed. Please install npm."
+  exit 1
+fi
+
+# Check if pm2 is installed
+if ! command -v pm2 &> /dev/null; then
+  echo "pm2 is not installed. Installing pm2..."
+  npm install -g pm2
+  if [ $? -eq 0 ]; then
+    echo "pm2 has been successfully installed."
+  else
+    echo "Failed to install pm2. Please install pm2 manually."
+    exit 1
+  fi
+fi
+
 # Check if uvicorn process is running on port 8000
 uvicorn_pid=$(lsof -ti :8000)
 
